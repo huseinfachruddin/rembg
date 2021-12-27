c55158f69e6f0e81045dcafb852091300ad5ffe2
imronpuji <imronpuji5@gmail.com>
2021-12-27 11:41:03 +0700

    imorn

--- controllers/Image.js
+++ controllers/Image.js
@@ -1,4 +1,5 @@
 const models = require('../models');
+const {link, folder, file} = require('rembg-nodejs');
 
 const initModel = async (cb) => {
 	const model = await models
@@ -6,10 +7,19 @@ const initModel = async (cb) => {
 	cb(initialized)
 }; 
 
-const save = () =>{}; 
-const remove = () =>{}; 
-const update = () =>{};
-const read = async (cb) => await cb(initModel(res=> res.findAll()))
+global.save = async (body, cb) => await initModel(model=>model.create(body).then(res=>cb(res))); 
 
-module.exports = {read} 
+global.read = async (cb) => await initModel(model=>model.findAll().then(res=>cb(res)))
+
+global.edit = async({name, id}, cb) => await initModel(model=>model.update({name}, {where:{id}}).then(res=>cb(res)));
+
+global.remove = async ({id}, cb) => await initModel(model=>model.destroy({where:{id}}).then(res=>cb(`berhasil menghapus`))); 
+
+// convert img
+global.link = async ({url, output},cb) => await  link(url,output, async (res)=>await cb(res));
+
+global.folder = async ({input, output},cb) => await  link(url,output, async (res)=>await cb(res));
+
+global.file = async ({input, output},cb) => await  file(input,output, async (res)=>await cb(res));
+
 
